19ae0ff370ab454625d1ecfd8d6bf403ec15ba98
Evan Danaher <github@edanaher.net>
2017-09-10 18:14:35 -0400

    Better show for Op/Command

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.List (intercalate)
+import Data.List (intercalate, mapAccumL)
 import Control.Monad (liftM)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as Char8
@@ -31,10 +31,25 @@ data Op =
   | Data B.ByteString
   | Copy Int Int
   | Label String
-  deriving  (Show, Eq)
+  deriving  (Eq)
 
-data Command = Com Op Int
-  deriving (Show, Eq)
+instance Show Op where
+  show op = case op of
+    Rep from len at final isdata -> "Rep from=" ++ show from ++ " len=" ++ show len ++ " at=" ++ show at ++ " final=" ++ show final
+    Print str final -> "Print " ++ show str ++ " final=" ++ show final
+    PrintLen len final isdata -> "Print " ++ show len ++ " final=" ++ show final
+    Data bytes -> "Data " ++ show bytes
+    Copy from len -> "Copy from=" ++ show from ++ " len=" ++ show len
+    Label name -> name ++ ":"
+
+data Command = Com Op Int Int Int Int
+  deriving (Eq)
+
+instance Show Command where
+  show (Com op size osize pos opos) = show pos ++ "=>" ++ show opos ++ "; +" ++ show size ++ "=>" ++ show osize ++ "  " ++ show op
+
+showProg :: [Command] -> String
+showProg coms = unlines $ map show coms
 
 instance Lua.FromLuaStack Op where
   peek idx = do
