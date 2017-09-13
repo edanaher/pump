3c6f990d2023c5d1620111e91416bf3c6873ce2f
Evan Danaher <github@edanaher.net>
2017-09-13 09:27:22 -0400

    Correct (but incomplete) simulation.

--- src/Simulate.hs
+++ src/Simulate.hs
@@ -12,7 +12,9 @@ simulateRep from len pos opos coms =
       base = takeWhile (\ (Com _ _ _ pos _) -> pos < from + len) start
       (coms', pos', opos') = foldl (\ (coms, pos, opos) (Com op size osize _ _) ->
           (Com op size osize pos opos:coms, pos + size, opos + osize)) ([], pos, opos) base
-  in trace ("--- Produced from " ++ show from ++ "/" ++ show len ++ " => " ++ show pos ++ "," ++ show opos ++ ":\n" ++ (unlines $ map show coms) ++ "--------" ) $ (reverse coms', 0, pos', opos')
+  in
+  --trace ("--- Produced from " ++ show from ++ "/" ++ show len ++ " => " ++ show pos ++ "," ++ show opos ++ ":\n" ++ (unlines $ map show $ reverse coms') ++ "--------" ) $
+  (reverse coms', 0, pos', opos')
 
 simulate' :: [Command] -> Int -> Int -> Int -> [Command] -> [Command]
 simulate' [] _ _ _ output = output
@@ -20,12 +22,14 @@ simulate' (Com op size osize pos opos:coms) printLen simpos simopos output =
   if printLen < 0 then error "printLen went negative on simulation!" else
   let (out, printLen', simpos', simopos') =
         if printLen > 0 then
-          ([Com op size osize simpos simopos], printLen - size, simpos + size, simopos + osize)
+          --trace ("Eating " ++ show op ++ " at " ++ show pos ++ "/" ++ show opos) $
+          ([Com op size osize simpos simopos], printLen - size, simpos + size, simopos + size)
         else
+          --trace ("simulating " ++ show op ++ " at " ++ show simpos ++ "/" ++ show simopos) $
           case op of
             Print str final -> ([Com (Data $ Char8.pack str) (length str) 0 simpos simopos], 0, simpos + length str, simopos)
             PrintLen len final -> ([], len, simpos, simopos)
-            Rep from len at final -> simulateRep from len pos opos output
+            Rep from len at final -> simulateRep from len simpos simopos output
             _ -> ([], 0, simpos, simopos)
   in simulate' coms printLen' simpos' simopos' (output ++ out)
 
