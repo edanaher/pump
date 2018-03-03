c141c90a3559f03f5c554dfb0e069e739a1e4c0d
Evan Danaher <github@edanaher.net>
2018-03-02 19:08:34 -0500

    build: Add lens, use nixpkgs hslua since it works now.

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let pkgs = import <nixpkgs> {};
     stdenv = pkgs.stdenv;
     my-haskell-pkgs = pkgs.haskell.packages.ghc802.extend (self: super: {
-      hslua_0_8_0 = super.hslua_0_8_0.override {
+      /*hslua_0_8_0 = super.hslua_0_8_0.override {
         lua5_1 = pkgs.lua5_3;
-      };
+        };*/
     });
 in
 my-haskell-pkgs.callPackage ./pump.nix { }
--- pump.nix
+++ pump.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, hslua_0_8_0, digest, HUnit, stdenv }:
+{ mkDerivation, base, hslua, digest, HUnit, stdenv, lens }:
 mkDerivation {
   pname = "pump";
   version = "0.1.0.0";
@@ -6,7 +6,7 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   testHaskellDepends = [ HUnit ];
-  executableHaskellDepends = [ base hslua_0_8_0 digest ];
+  executableHaskellDepends = [ base hslua digest lens ];
   description = "Compiler targeting deflate compressed streams (gzip, zip, etc.)";
   license = stdenv.lib.licenses.bsd3;
   postInstall =
