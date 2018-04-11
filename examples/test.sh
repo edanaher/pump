557a98dbc43723ab8469eb8ff8f4a74e985aa1ad
Evan Danaher <github@edanaher.net>
2018-04-10 22:13:55 -0400

    examples/test: Check simulations work right.  Now tweedledee fails.
    I think that's true?

--- examples/test.sh
+++ examples/test.sh
@@ -15,18 +15,27 @@ if [[ "$1" == narcissus || "$1" == "" ]]; then
   diff -u <(echo $MD5) <(echo $MD5b)
   diff -u $EXAMPLES/narcissus.in $EXAMPLES/narcissus.out
 
+  $PUMP $EXAMPLES/narcissus.out -o $EXAMPLES/narc.gz
+  diff -u $EXAMPLES/narcissus.gz $EXAMPLES/narc.gz
+
   echo "Narcissus successful"
-  rm $EXAMPLES/narcissus{.in,.out,.gz}
+  rm $EXAMPLES/narcissus{.in,.out,.gz} $EXAMPLES/narc.gz
 fi
 
 if [[ "$1" == tweedledee || "$1" == "" ]]; then
-  $PUMP $EXAMPLES/tweedledee.lua
+  $PUMP -s $EXAMPLES/tweedledee.lua -s
   MD5=$(md5sum $EXAMPLES/tweedledee.gz)
   gunzip -N $EXAMPLES/tweedledee.gz
+  MD5dum=$(md5sum $EXAMPLES/tweedledum.gz)
   gunzip -N $EXAMPLES/tweedledum.gz
   MD5b=$(md5sum $EXAMPLES/tweedledee.gz)
   diff -u <(echo $MD5) <(echo $MD5b)
-  rm $EXAMPLES/tweedledee.gz
+
+  $PUMP $EXAMPLES/tweedledee.out -o $EXAMPLES/tweedledum.gz
+  MD5dumb=$(md5sum $EXAMPLES/tweedledum.gz)
+  diff -u <(echo $MD5dum) <(echo $MD5dumb)
+
+  rm $EXAMPLES/tweedledee.{gz,in,out} $EXAMPLES/tweedledum.gz
 
   echo "Tweedledee successful"
 fi
