00baba9adbe809f560449f331e38e89e692ca7bf
Evan Danaher <github@edanaher.net>
2018-04-10 20:27:30 -0400

    Try to back-substitute copies in.  Mostly works, maybe?

--- src/Render.hs
+++ src/Render.hs
@@ -3,11 +3,12 @@ module Render where
 import qualified Data.ByteString as B
 import qualified Data.Char as Char
 import qualified Data.ByteString.Char8 as Char8
-import Data.List (intercalate)
+import Data.List (intercalate, nub)
 import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust)
 import Data.Map.Strict (Map)
 import Text.Printf (printf)
+import Debug.Trace (trace)
 
 import Control.Lens ((^.), (&), (.~))
 
@@ -75,6 +76,9 @@ renderCom origsrc com = case com ^. op of
   DataInt value size ->
     let args = renderArgs $ Map.fromList $ [("int", LAddress value), ("size", LInt size)]
     in "data " ++ args
+  Copy from len ->
+    let args = renderArgs $ Map.fromList $ [("from", LAddress from), ("len", LAddress len)]
+    in "data " ++ args
   _ -> show $ com ^. op
 
 
@@ -98,15 +102,59 @@ alignFields labels input output =
 
     _ -> output
 
+checkCopy [] _ _ _ = []
+checkCopy (h:r) copysrc start n = case h ^. src of
+  SrcCopy copysrc' i count | copysrc' == copysrc && i == n ->
+    if i == count - 1
+    then [(copysrc, start)]
+    else checkCopy r copysrc start (n + 1)
+  _ -> []
 
-cleanup :: LabelMap -> [Command] -> [Command] -> [Command]
+collapseCopies labels inputs outputs =
+  let copies = map (^. src) $ filter (\com -> case com ^. src of SrcCopy _ _ _ -> True; _ -> False) inputs
+      copyIndices = nub $ map (\ (SrcCopy (SrcedOp (Copy from len, _)) _ _) -> (from, len)) copies
+      dropLabels = filter (\com -> case com ^. op of Label _ -> False; _ -> True)
+      copyCandidates :: [((Address, Address), [Command])]
+      copyCandidates = concatMap (\ (start, len) ->
+          let suffix = dropWhile (\com -> com ^. pos /= start @!labels) outputs
+              range = takeWhile (\com -> com ^. pos <= start @! labels + len @! labels) suffix
+          in if range == [] || (last range ^. pos /= start @! labels + len @! labels)
+            then []
+            else [((start, len), dropLabels (init range))]) copyIndices
+      startsCopy (SrcCopy _ 0 _) = True
+      startsCopy _ = False
+      getSrc (SrcCopy (SrcedOp (op, _)) _ _) = op
+      prefixMatch _ [] = True
+      prefixMatch [] _ = False
+      prefixMatch (c:cs) (t:ts) = trace ("Prefix matching\n" ++ show c ++ "\n" ++ show t ++ "\n") c ^. op == t ^. op && prefixMatch cs ts
+      substCopies input output = case (input, output) of
+        (i:input', o:output') ->
+          if startsCopy (i ^. src) then
+            let copies = filter (prefixMatch input . snd) copyCandidates in
+            case copies of
+              [] -> let (input'', output'') = substCopies input' output'
+                    in trace "Didn't find matching copy" (i:input'', o:output'')
+              (((start, len), copy):_) ->
+                   let (input'', output'') = substCopies (drop (length copy) input) (drop (length copy) output)
+                   in trace "Found matching copy" (Com (getSrc $ i ^. src) SrcNone 0 0 0 0:input'',
+                                                   Com (Copy start len) SrcNone 0 0 0 0:output'')
+
+          else
+            let (input'', output'') = substCopies input' output'
+            in (i:input'', o:output'')
+        _ -> (input, output)
+--  in trace ("copies are \n" ++ unlines (map (\(src, lines) -> show src ++ " => \n" ++ unlines (map show lines) ++ "\n\n") copies)) outputs
+  in trace ("Copy indices: " ++ show copyIndices ++ " => candidates " ++ unlines (map (\((start, len), coms) -> show start ++ "+" ++ show len ++ "\n" ++ unlines (map show coms) ++ "\n" ) copyCandidates)) substCopies inputs outputs
+
+cleanup :: LabelMap -> [Command] -> [Command] -> ([Command], [Command])
 cleanup labels inputs outputs =
   let paired = zip (inputs ++ repeat (Com (Padding 0) SrcNone 0 0 0 0)) outputs -- This should be smarter
       cleaned = map (uncurry $ alignFields labels) paired
+      decopied = collapseCopies labels inputs cleaned
   in
-  cleaned
+  decopied
 
 renderProgram :: [String] -> LabelMap -> [Command] -> [Command] -> Bool -> ([String], [String])
 renderProgram srcs labels inputs outputs rawsim =
-  let outputs' = if rawsim then outputs else cleanup labels inputs outputs
-  in (map (render srcs) inputs, map (render srcs) outputs')
+  let (inputs', outputs') = if rawsim then (inputs, outputs) else cleanup labels inputs outputs
+  in (map (render srcs) inputs', map (render srcs) outputs')
